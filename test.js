403e2b43f508c022848dd77459f76d239dcd2dc5
Peter bock <bockpeter@hotmail.com>
2017-11-04 17:44:57 +0100

    psuedo-code kernel applier
    Also had several problems when trying to generalize the zero-padding function to any uneven kernel size (3X3, 5X5, 7X7, 9X9...)

--- test.js
+++ test.js
@@ -28,8 +28,23 @@ let beforeArray = [[1,2,3],[1,2,3],[1,2,3],[1,2,3],[1,2,3]];
 // STEP 1: zero-padding step
 
 // warning -> this only works for 3X3 kernels.
-// I'm having enormous difficulties figuring out how to apply it so that we get the correct number of starting and ending zero arrays, if we specify a specific array size...
 
+// I'm having enormous difficulties figuring out how to apply it so that we get the correct number of starting and ending zero
+//arrays, if we specify a specific kernel size...
+// I can modify it to eliminate the need for parArray2DEnds,
+//to add as many 0 to the end of each subarray as needed depending on Kernel size too.
+// problem is, I can't figure out how to functionally apply the padArray function
+//with complete Zero arrays to the beginning and end of the ImageArray
+
+// so as to have this:
+
+// Kernel 3X3 -> 1 extra line of zeros at beginning and end
+// Kernel 5X5 -> 2 extra lines of zeros at beginning and end.
+// Etc.,
+
+// my initial idea involved using padArray() a total of (Math.floor(KernelSize/2)) times,
+//but I don't know how to repeat the function on the result of it's prior application an undefined number of times 
+//HELP
 
 
 // FUNCTIONAL: fonction ajoutant un zero avant et apres un array donnee
@@ -68,3 +83,49 @@ alert(displayArray);
 // -----------------------------------------------------------------------------
 
 // convolution function
+
+let paddedTestArray = padImageArray(beforeArray,3);
+
+let testKernel = [[0,0,0],[0,1,0],[0,0,0]];
+
+// AKA:
+// 0 0 0
+// 0 1 0
+// 0 0 0
+
+//
+////              Kernel Function PseudoCode
+//
+
+//given that each subarray in our ImageArray is 1 line, 
+//and that the number of subarrays in our kernel define how many lines it spans:
+
+
+// we need an Y_Array "iterator" array with Num of the beginning and end line, <- 1 FunctA(kernelSize)
+//of the length of the number of lines in our kernel.
+
+// EX: [0,1,2] at the start, meaning first col, second col, third column
+
+// an X_Array "iterator" array that spans the number of elements in one line of our kernel. <- 1 FunctA(kernelSize)
+
+// Ex: [0,1,2] at the start, meaning first line, second line, third line
+
+// each of these arrays are used as-is for 1 run of the kernel, 
+//then assign the resulting value to the correct position in modified image array
+
+// then X_Array++,
+//                and re-run the kernel, untill x_Array gets to (lenImgArray - lenKernel)
+// then Y_Array++,
+//                begin with X_array at 0 again to (lenImgArray - lenKernel)...
+
+// during 1 kernel run: <- 1 FunctB to create it ? or more ?
+
+//kernel applies a calculation for each position (i)(j) taken from the 2 X and Y arrays.
+// calculation is:
+//   modifiedImageArray[i][j] = ImageArray[i][j] * kernel[indexOf(i,X_array)][indexOf(j,Y_array)]
+
+
+// at the end, we should have a transformedImageArray corresponding to the application of the kernel every pixel of the TestArray.
+
+
+// hmmm... can't think of more that's needed to complete the psuedo-code ?
