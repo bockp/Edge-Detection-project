9332dd72aa1a9b370e84e3ea2b108cb495ee46df
CeciliaOstertag <cecilia.m.ostertag@gmail.com>
2018-02-08 16:24:58 +0100

    test with 8bit, 16bit, float32

--- examples/testCanny.js
+++ examples/testCanny.js
@@ -1,20 +1,29 @@
 let uint8_lena = lenna_256x256;
 let uint16_lena = lenna_256x256.map ( (px) => px * 256);
-let float_lena = lenna_256x256.map( (px) => px/128 - 1.0);
- 
-let type = 'uint8';
-let pix = uint8_lena;
+let float_lena = lenna_256x256.map( (px) => px/256 );
 
-let img = new T.Image(type,256,256);
+//let type = 'uint16';
+//let pix = uint16_lena;
+let W = 256;
+let H = 256;
+//128-105 , 300-246 , 512-420 , 1024-840 , 2048-1679, 4096-3360
+let img = new T.Image('uint8',W,H);
 img.setPixels(new Uint8Array(uint8_lena));
-
 let gpuEnv = gpu.getGraphicsContext("preview");
-
 gpuDisplay(img.getRaster(),gpuEnv);
 
-let img3 = new T.Image(type,256,256);
-img3.setPixels(new Uint8Array(pix));
+let img1 = new T.Image('uint8',W,H);
+img1.setPixels(new Uint8Array(uint8_lena));
+let gpuEnv1 = gpu.getGraphicsContext("preview1");
+gpuEdgeCanny(50.0,100.0)(img1.getRaster(),gpuEnv1);
 
-let gpuEnv3 = gpu.getGraphicsContext("preview2");
+let img2 = new T.Image('uint16',W,H);
+img2.setPixels(new Uint16Array(uint16_lena));
+let gpuEnv2 = gpu.getGraphicsContext("preview2");
+gpuEdgeCanny(50.0,100.0)(img2.getRaster(),gpuEnv2);
+
+let img3 = new T.Image('float32',W,H);
+img3.setPixels(new Float32Array(float_lena));
+let gpuEnv3 = gpu.getGraphicsContext("preview3");
 gpuEdgeCanny(50.0,100.0)(img3.getRaster(),gpuEnv3);
 
