fd6278a8d0ed99175c7968ba050c0cf33a69fe8d
OphelieThierryPeulson <ophelie.thierry@gmail.com>
2018-02-16 16:16:48 +0100

    Update testEdge.js

--- examples/testEdge.js
+++ examples/testEdge.js
@@ -2,11 +2,8 @@ let uint8_lena = lenna_256x256;
 let uint16_lena = lenna_256x256.map ( (px) => px * 256);
 let float_lena = lenna_256x256.map( (px) => px/256 );
 
-//let type = 'uint16';
-//let pix = uint16_lena;
 let W = 256;
 let H = 256;
-//128-105 , 300-246 , 512-420 , 1024-840 , 2048-1679, 4096-3360
 let img = new T.Image('uint8',W,H);
 img.setPixels(new Uint8Array(uint8_lena));
 let gpuEnv = gpu.getGraphicsContext("preview");
